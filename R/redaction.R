6ee6944e36a07f23bca15ec86a02e4206f3a1848
Tom Palmer <remlapmot@hotmail.com>
2022-07-20 15:24:13 +0100

    Fix typos in fn names, add links, add fullstops, formatting

--- R/redaction.R
+++ R/redaction.R
@@ -433,14 +433,14 @@ redacted_summary_catnum <- function(
 # functions to convert redacted sumary tables to gt objects ----
 
 
-#' Convert output of categorical tabulation (redact_summary_cat) to gt object
+#' Convert output of categorical tabulation ([redacted_summary_cat]) to gt object
 #'
-#' @param x The data.frame produced by `redact_summary_cat`
-#' @param var_name The variable name
-#' @param pct_decimals Decimal precision for percentages
-#' @return A gt object
+#' @param x The data.frame produced by [redacted_summary_cat].
+#' @param var_name The variable name.
+#' @param pct_decimals Decimal precision for percentages.
+#' @return A gt object.
 #'
-#' @details This function takes the output of `redact_summary_cat` and converts it to a gt object (as from the `gt` package) for outputting to html/pdf.
+#' @details This function takes the output of [redacted_summary_cat] and converts it to a gt object (as from the gt package) for outputting to html/pdf.
 #'
 #' @export
 gt_cat <- function(
@@ -478,17 +478,17 @@ gt_cat <- function(
 }
 
 
-#' Convert output of categorical cross-tabulation (redact_summary_cat_cat) to gt object
+#' Convert output of categorical cross-tabulation ([redacted_summary_catcat]) to gt object
 #'
-#' @param x The data.frame produced by `redact_summary_catcat`
-#' @param var1_name The name of the first categorical variable
-#' @param var2_name The name of the second categorical variable
-#' @param title The title of the table
-#' @param source_note A footnote
-#' @param pct_decimals Decimal precision for percentages
-#' @return A gt object
+#' @param x The data.frame produced by [redacted_summary_catcat].
+#' @param var1_name The name of the first categorical variable.
+#' @param var2_name The name of the second categorical variable.
+#' @param title The title of the table.
+#' @param source_note A footnote.
+#' @param pct_decimals Decimal precision for percentages.
+#' @return A gt object.
 #'
-#' @details This function takes the output of `redact_summary_catcat` and converts it to a gt object (as from the `gt` package) for outputting to html/pdf.
+#' @details This function takes the output of [redacted_summary_catcat] and converts it to a gt object (as from the gt package) for outputting to html/pdf.
 #'
 #' @export
 gt_catcat <- function(
@@ -610,16 +610,16 @@ gt_num <- function(
 }
 
 
-#' Convert output of categorical-numeric cross-tabulation (redact_summary_catnum) to gt object
+#' Convert output of categorical-numeric cross-tabulation ([redacted_summary_catnum]) to gt object
 #'
-#' @param x The data.frame produced by `redact_summary_catnum`
-#' @param cat_name The categorical variable name
-#' @param num_name The numeric variable name
-#' @param num_decimals Decimal precision for numbers
-#' @param pct_decimals Decimal precision for percentages
-#' @return A gt object
+#' @param x The data.frame produced by [redacted_summary_catnum].
+#' @param cat_name The categorical variable name.
+#' @param num_name The numeric variable name.
+#' @param num_decimals Decimal precision for numbers.
+#' @param pct_decimals Decimal precision for percentages.
+#' @return A gt object.
 #'
-#' @details This function takes the output of `redact_summary_catnum` and converts it to a gt object (as from the `gt` package) for outputting to html/pdf.
+#' @details This function takes the output of [redacted_summary_catnum] and converts it to a gt object (as from the gt package) for outputting to html/pdf.
 #'
 #' @export
 gt_catnum <- function(
